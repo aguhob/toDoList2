e87b8090134a2ec27f51ecf482da62437a6e06a2
Minda Aguhob <ma@peakfoqus.com>
2018-04-15 02:15:22 +0000

    connected mongo, viz changes, new git

---
+++ server.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// MongoDB Stuff
+var mongoose = require('mongoose');
+var url = 'mongodb://user:user@ds137207.mlab.com:37207/notesdb34';
+
+mongoose.connect(url, function(error) {
+    if (error){
+        console.log("Sorry something happened: " + error);
+    } else {
+        console.log("Connected to MongoDB database.")
+    }
+});
+
+var Schema = mongoose.model('Notes', {
+    notes: String
+});
+
+// Express 'Routes'
+app.use('/client', express.static('client/'));
+app.use('/js', express.static('client/js'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+http.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
+
+app.get('/getNotes', function (request, result) {
+    console.log("Data requested, and sent.");
+    
+    Schema.find({}, function (error, notes) {
+            if (error) {
+                console.log("Sorry we had issues searching the database: " + error)
+            } else {
+                result.send(notes);
+            }
+        });
+});
+
+app.post('/deleteNote', function (request, result) {
+    var noteId = request.body._id;
+    
+    // below code was used to test deleting with a string
+    // var objectToDelete = request.body._id;
+    
+    // objectToDelete = {
+    //     _id: { $oid: objectToDelete}
+    // }
+    // console.log(objectToDelete);  
+    Schema.findByIdAndRemove(noteId, function (error, todo){
+        if (error) {
+            console.log("sorry couldn't find anything or error >" + error);
+        } else {
+            console.log("doc deleted.");
+            result.send(200);
+        }
+        
+    });
+    
+});
+
+app.post('/submitNote', function (request, result) {
+    console.log(request.body);
+    
+    var newNote = Schema(request.body);
+    newNote.save(function (error) {
+    if (error) {
+        console.log("Sorry the document was not saved: " + error);
+    } else {
+        console.log("Document was saved!");
+        
+        Schema.find({}, function (error, notes) {
+            if (error) {
+                console.log("Sorry we had issues searching the database: " + error)
+            } else {
+                io.emit('important', notes);
+            }
+        });
+    }
+});
+    
+    result.send(200);
+});
+
+console.log("hello");
